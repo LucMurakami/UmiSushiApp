64807ba719f84b0537d6d5029eb01f63e1c22d58
Luc Murakami <luc.murakami@hotmail.com>
2017-12-17 14:46:26 -0800

    Added Submenus

--- app/src/main/java/shasha/example/com/umisushiapp/SubMenus.java
+++ app/src/main/java/shasha/example/com/umisushiapp/SubMenus.java
@@ -8,7 +8,11 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class Menu extends AppCompatActivity {
+/**
+ * Created by LucMurakami on 2017-12-17.
+ */
+
+public class SubMenus extends AppCompatActivity {
 
     ListView myListView;
     String[] items;
@@ -17,11 +21,11 @@ public class Menu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.sashimi);
 
         Resources res = getResources();
-        myListView = (ListView) findViewById(R.id.myListView);
-        items = res.getStringArray(R.array.items);
+        myListView = (ListView) findViewById(R.id.myListViewSubMenu);
+        items = res.getStringArray(R.array.sashimi_items);
         descriptions = res.getStringArray(R.array.descriptions);
 
         ItemAdapter itemAdapter = new ItemAdapter(this, items, descriptions);
@@ -30,7 +34,7 @@ public class Menu extends AppCompatActivity {
         myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent showDetailActivity = new Intent(getApplicationContext(), SubMenus.class);
+                Intent showDetailActivity = new Intent(getApplicationContext(), DetailActivity.class);
                 showDetailActivity.putExtra("shasha.example.com.ITEM_INDEX", i);
                 startActivity(showDetailActivity);
             }
